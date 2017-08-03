e2bf8f0487034dba642b3092cb90b1e736541ff6
Jonathan Martin <me@nybblr.com>
2017-08-03 14:12:07 -0500

    Speed up zshmarks.

--- zsh/zshmarks.zsh
+++ zsh/zshmarks.zsh
@@ -1,22 +1,11 @@
 # Autoadd jump bookmarks to hash
 load_zshmarks() {
-	zshmarks=$(egrep -o "[^:]+\:[[:space:]]*.+" $HOME/.zshmarks)
-	echo $zshmarks | while read zmark
-	do
-		# sed s/([^:]+)\:[[:space:]]*.+/\1/'
-		name=$(echo $zmark | egrep -o '^[^:]+')
-
-		# Expand directory path
-		directory=$(echo $zmark | egrep -o '([^: ])+$')
-		eval directory=$directory
-
-		eval "hash -d $name=\"$directory\""
-	done
+  source $HOME/.zshmarks
 }
 
 function b() {
 	# Use ZSH to expand current path with hash table
-	echo "$1: `print -P %~`" >> ~/.zshmarks && load_zshmarks
+	echo "hash -d $1=`print -P %~`" >> ~/.zshmarks && load_zshmarks
 }
 
 load_zshmarks
